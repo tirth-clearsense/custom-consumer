372cd65b8c647cc9c436a1efc654d4c1ed3652f3
tirth-clearsense <tpatel@clearsense.com>
2022-02-28 14:19:32 -0600

    add logging

--- recv.py
+++ recv.py
@@ -1,21 +1,21 @@
 import asyncio
-import datetime
 import json
 from urllib.request import CacheFTPHandler
 from azure.eventhub.aio import EventHubConsumerClient
 from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
 import logging
 import os
-from avro_validator.schema import Schema
 from fastavro.validation import validate
 from fastavro import parse_schema
 from postgresdb import *
 from configparser import ConfigParser
 
-# from sqlitedb import conn,heartrate
-# import sqlalchemy as db 
-
-LOG = logging.getLogger() 
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename = "customconsumer.log",
+                    filemode = "w",
+                    format = Log_Format, 
+                    )
+logger = logging.getLogger() 
 
 config_object = ConfigParser()
 config_object.read("config.ini")
@@ -31,39 +31,43 @@ with open(data_dict_path, 'r') as fi:
 session = loadSession()
 
 # map table name to correcspoding model class name 
-table_to_class = {'heartrate': Heartrate, 'heart_intensity_minutes': '', 'active_minutes': '', 'resting_calories': '','active_calories':'',
+table_to_model = {'heartrate': Heartrate, 'heart_intensity_minutes': '', 'active_minutes': '', 'resting_calories': '','active_calories':'',
 'total_calories': '', 'distance': '', 'weight': '', 'body_fat': '', 'height':'', 'location': '', 'speed': '', 'blood_glucose': '',
 'body_temperature':''
 }
 
-# function to return model class name
+# function to return model from class name
 def getClass(table_name):
-    return table_to_class[table_name]
+    return table_to_model[table_name]
 
+# function to get the table name from personicle data dictionary
+# input params: stream type 
+def getTableName(stream_type):
+    personicle_data_type = stream_type.split(".")
+    return personcile_data_types_json["com.personicle"]["individual"]["datastreams"][personicle_data_type[-1]]["TableName"]
+
+# function to store the incoming events to postgres database
 async def on_event(partition_context, event):
     # Print the event data.
-    
     print("Received the event: \"{}\" from the partition with ID: \"{}\"".format(event.body_as_json(encoding='UTF-8'), partition_context.partition_id))
     
     try:
         objects=[] # for bulk inserts
         current_event = event.body_as_json(encoding='UTF-8')
         stream_type = current_event['streamName']
-        personicle_data_type = stream_type.split(".")
-        table_name = personcile_data_types_json["com.personicle"]["individual"]["datastreams"][personicle_data_type[-1]]["TableName"]
-        #
-        print(table_name)
-
-        print(f"stream type is {stream_type}")
+        
         file_path = os.path.join(script_dir, f"avro/{stream_type}"+".avsc")
         
-        print(f"file path is {file_path}")
         with open(file_path, 'r') as fi:
             schema = json.load(fi)
         
+        # validate the event with avro schema
         parsed_schema = parse_schema(schema) 
         validate(current_event, parsed_schema)
         print("Valid event")
+
+        # if valid, get the table name and store data
+        table_name = getTableName(stream_type)
 
         individual_id = current_event['individual_id']
         source=current_event['source']
@@ -80,8 +84,9 @@ async def on_event(partition_context, event):
         session.bulk_save_objects(objects)
         session.commit()
         
-    except:
-        print("Invalid event: \"{}\" from the partition with ID: \"{}\"".format(event.body_as_json(encoding='UTF-8'), partition_context.partition_id))
+    except Exception as e:
+        # print("Invalid event: \"{}\" from the partition with ID: \"{}\"".format(event.body_as_json(encoding='UTF-8'), partition_context.partition_id))
+        logger.error(e)
         pass
     
     # Update the checkpoint so that the program doesn't read the events
@@ -89,7 +94,7 @@ async def on_event(partition_context, event):
     await partition_context.update_checkpoint(event)
 
 async def main():
-      
+
     # Create an Azure blob checkpoint store to store the checkpoints.
     checkpoint_store = BlobCheckpointStore.from_connection_string("{checkpoint}".format(checkpoint=azureblob["CHECKPOINT"]), 
                             "{container}".format(container=azureblob["CONTAINER"]))
