9cfb19e2822281c948e480c60b4d6fa10a4e1b44
Vaibhav Pandey <97196207+vaibhav-clearsense@users.noreply.github.com>
2022-06-27 22:09:21 -0700

    added schema for interval data types

--- base_schema.py
+++ base_schema.py
@@ -9,9 +9,27 @@ base_schema = {
         "unit": Column(String),
         "confidence": Column(String, default=None)
         },
+    "integer_interval_datastream_schema.avsc": {
+        "individual_id": Column(String, primary_key=True),
+        "start_time": Column(TIMESTAMP, primary_key=True),
+        "end_time": Column(TIMESTAMP, primary_key=True),
+        "source": Column(String, primary_key=True),
+        "value": Column(Integer),
+        "unit": Column(String),
+        "confidence": Column(String, default=None)
+        },
     "numeric_datastream_schema.avsc": {
         "individual_id": Column(String, primary_key=True),
         "timestamp": Column(TIMESTAMP, primary_key=True),
+        "source": Column(String, primary_key=True),
+        "value": Column(Float),
+        "unit": Column(String),
+        "confidence": Column(String, default=None)
+        },
+    "numeric_interval_datastream_schema.avsc": {
+        "individual_id": Column(String, primary_key=True),
+        "start_time": Column(TIMESTAMP, primary_key=True),
+        "end_time": Column(TIMESTAMP, primary_key=True),
         "source": Column(String, primary_key=True),
         "value": Column(Float),
         "unit": Column(String),
